dd50946487bf25967a229db432ef2d822e3b7304
Brenden Thomas <brenden.thomas17@gmail.com>
2022-06-25 12:15:02 -0500

    -Added ability to remove tasks -tasks now saved to local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Overview from './components/Overview';
 import {useState} from'react';
@@ -7,9 +6,29 @@ import {useState} from'react';
 
 function App() {
   const [first, setfirst] = useState([])
+
+  window.onload =()=>{
+    renderAllTasks();
+  }
+
   const addTask =() =>{
     const taskName = document.querySelector('#inputField').value;
-    setfirst(newTask => [...newTask, taskName])
+    let taskObj ={
+      name: taskName,
+      id: first.length
+    }
+    setfirst(newTask => [...newTask, taskObj])
+    localStorage.setItem(taskObj.name, JSON.stringify(taskObj));
+  }
+
+
+  const renderAllTasks =()=>{
+
+    let tempTaskArr = [];
+    for( let i=0; i <localStorage.length; i++){
+      tempTaskArr.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
+    }
+    setfirst([...tempTaskArr])
   }
 
   return (
@@ -17,7 +36,7 @@ function App() {
       
       <input id="inputField" type="text" placeholder="Enter Task Here"></input>
       <button type="submit" onClick={addTask}>Submit</button>
-      <Overview key= {first.length} tasks={first}/>
+      <Overview tasks={first} renderAllTasks={renderAllTasks} />
     </div>
   );
 }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -3,21 +3,24 @@ import './Overview.css'
 
 
 
-const Overview = ({tasks})=>{
+const Overview = ({tasks, renderAllTasks})=>{
 
+    const removeTask =(selectedTask) =>{
+
+            localStorage.removeItem(selectedTask.name)
+            renderAllTasks();
+    }
 
     return(
 
         
         <div id="holder">
-        <ul>
          {tasks.map((poke)=>   
-                <div className="taskDiv"> 
-                    <input type="checkbox" ></input>
-                <p>{poke}</p>
+                <div className="taskDiv" key={poke.id}> 
+                    <input type="checkbox" id={poke.id} onChange={() => removeTask(poke)}></input>
+                <p>{poke.name}</p>
                 </div>
          )}
-        </ul>   
         </div>
         
     );
